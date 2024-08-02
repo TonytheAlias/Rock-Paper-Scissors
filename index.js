9bfcfcc4d76aac806a2afd7f97ebf393b061203a
TonytheAlias <tyreekbrasfield@gmail.com>
2024-08-02 15:52:58 -0500

    UI rock paper scissor

--- index.js
+++ index.js
@@ -14,8 +14,22 @@ function getHumanChoice(){
 
 let humanScore = 0; 
 let computerScore = 0; 
+const hScore = document.createElement('p'); 
+const cScore = document.createElement('p'); 
+let GameOver = document.createElement('p');
 
 function playRound(humanChoice, computerChoice){
+
+    hScore.textContent = humanScore;
+    cScore.textContent = computerScore; 
+    
+    if(humanScore == 5)
+        {
+            GameOver.textContent = "You Won!! Game over"
+        }
+        else if (computerScore == 5){
+            GameOver.textContent = "You Lost!! Game Over"
+        }
 
     if (humanChoice === computerChoice) {
         return "Draw! Try Again";
@@ -25,30 +39,56 @@ function playRound(humanChoice, computerChoice){
         (humanChoice === "Scissor" && computerChoice === "Paper")
     ) {
         humanScore++;
-        return `You Win! ${humanChoice} beats ${computerChoice}`;
+        return `You Win! ${humanChoice} beats ${computerChoice}.`;
     } else {
         computerScore++;
-        return `You lose! ${computerChoice} beats ${humanChoice}`;
+        return `You lose! ${computerChoice} beats ${humanChoice}.`;
     }
+
+
 }
 
-function playGame(){
-    
-    for(let i = 1; i <= 5; i++){
-        const humanSelection = getHumanChoice(); 
+        const btnRock = document.createElement('button');
+        btnRock.textContent = 'Rock'
+        const btnPaper = document.createElement('button');
+        btnPaper.textContent = 'Paper'
+        const btnScissor = document.createElement('button');
+        btnScissor.textContent = 'Scissor'
+
+        const result = document.createElement('span')
+        
+        btnRock.addEventListener('click', () =>{
+        const humanSelection = 'Rock'; 
         const computerSelection = getComputerChoice();
-       
-       console.log(playRound(humanSelection, computerSelection))
-       console.log(`Human Score: ${humanScore}`)
-       console.log(`Computer Score: ${computerScore}`)
-    }
-    if(humanScore > computerScore)
-        {
-            console.log("You Won!! Game over")
-        }
-        else{
-            console.log("You Lost!! Game Over")
-        }
-}
 
-playGame()
+        result.textContent = playRound(humanSelection, computerSelection)
+        });
+
+        btnPaper.addEventListener('click', () =>{
+        const humanSelection = 'Paper'; 
+        const computerSelection = getComputerChoice();
+
+        result.textContent = playRound(humanSelection, computerSelection)
+        });
+
+        btnScissor.addEventListener('click', () =>{
+        const humanSelection = 'Scissor'; 
+        const computerSelection = getComputerChoice();
+
+        result.textContent = playRound(humanSelection, computerSelection)
+        });
+
+        const body = document.querySelector('body')
+        const div = document.createElement('div'); 
+        body.appendChild(div);
+
+        div.appendChild(btnRock); 
+        div.appendChild(btnPaper); 
+        div.appendChild(btnScissor); 
+
+
+        div.appendChild(hScore);
+        div.appendChild(cScore);
+        div.appendChild(result); 
+        div.appendChild(GameOver); 
+        
